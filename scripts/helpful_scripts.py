dad8ca36222f478d596106f6e3c50b173ad0f85e
Jack-Tilley <tilley.e.jack@gmail.com>
2022-02-17 10:08:58 -0500

    up

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -8,6 +8,7 @@ STARTING_PRICE = 200000000000
 
 
 def get_account():
+
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
@@ -21,5 +22,6 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {
+                                "from": get_account()})
     print("Mocks Deployed.")
